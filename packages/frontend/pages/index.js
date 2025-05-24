09d19d865f36d33431272e26d463029b195b0ae7
Opensources591 <fomokaro23@gmail.com>
2025-05-24 11:54:58 +0100

    Create index.js
    feat: add frontend home page

---
+++ packages/frontend/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold">Welcome to KitoSwap Dex (KSD)!</h1>
+    </div>
+  );
+}
